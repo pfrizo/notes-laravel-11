c0e9a40caa0a3dfb906e2997cb28234c8b23d766
pfrizo <pedrofrizo@hotmail.com>
2025-01-10 13:46:40 -0300

    feat: implement edit and delete notes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,8 @@ class MainController extends Controller
 {
     public function index(){
         $id = session('user.id');
-        $notes = User::find($id)->notes()->orderBy('id', 'desc')->get()->toArray();
+        //$notes = User::find($id)->notes()->whereNull('deleted_at')->orderBy('updated_at', 'desc')->get()->toArray();
+        $notes = User::find($id)->notes()->orderBy('updated_at', 'desc')->get()->toArray();
 
         return view('home', ['notes' => $notes]);
     }
@@ -43,12 +44,59 @@ class MainController extends Controller
     public function editNote($id){
         $id = Operations::decryptId($id);
         
-        echo "I'm editing note with id = $id";
+        $note = Note::find($id);
+
+        return view('edit_note', ['note' => $note]);
+    }
+
+    public function editNoteSubmit(Request $request){
+        $request->validate([
+            'text_title' => 'required|min:3|max:200',
+            'text_note' => 'required|min:3|max:300'
+        ]);
+
+        if($request->note_id == null){
+            return redirect()->route('home');
+        }
+
+        $id = Operations::decryptId($request->note_id);
+
+        $note = Note::find($id);
+
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
     }
 
     public function deleteNote($id){
         $id = Operations::decryptId($id);
 
-        echo "I'm deleting note with id = $id";
+        $note = Note::find($id);
+
+        return view('delete_note', ['note' => $note]);
+    }
+
+    public function deleteNoteConfirm($id){
+        
+        $id = Operations::decryptId($id);
+
+        $note = Note::find($id);
+
+        //Hard delete
+        //$note->delete();
+
+        //soft delete (add whereNull('deleted_at') in index query)
+        //$note->deleted_at = date('Y:m:d H:i:s');
+        //$note->save();
+
+        //hard delete (property in model)
+        //$note->forceDelete();
+
+        //soft delete (property in model)
+        $note->delete();
+
+        return redirect()->route('home');
     }
 }
